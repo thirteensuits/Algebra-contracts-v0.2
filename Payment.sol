14db0465c9b615578cd92b2b7d55c7de9bc162a0
thirteensuits <110221153+thirteensuits@users.noreply.github.com>
2022-10-04 20:18:18 +0800

    Update Payment.sol

--- Payment.sol
+++ Payment.sol
@@ -1,144 +1,66 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-only
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/thirteensuits/Algebra-contracts-v0.2/blob/main/TrunkNFT.sol";
 
-contract BranchNFT is ERC721Enumerable, Ownable {
-    ERC721 internal immutable nft;
-
-    using Strings for uint256;
+contract PaymentSplitter is Context {
+    event PaymentReleased(address to, uint256 amount);
+    event PaymentReceived(address from, uint256 amount);
+    TrunkNFT internal nft;
+    uint256 private _totalReleased;
+    uint256 public number;
+    mapping(uint256 => uint256) private _released;
+    address payable account;
     
-    string public baseURI;
-    string public baseExtension = ".json";
-    uint256 public cost = 1 ether;
-    uint256 public maxMintAmount = 10;
-    bool public paused = false;
-    address payable payment;
-
-
-    constructor(
-        ERC721 _nft,
-        string memory _name,
-        string memory _symbol,
-        string memory _initBaseURI,
-        address _payment
-    ) ERC721(_name, _symbol) {
-        nft = _nft;
-        setBaseURI(_initBaseURI);
-        payment = payable(_payment);
+    constructor(TrunkNFT _nft) payable {
+    nft = _nft;
+    account = payable(msg.sender);
+    number = nft.maxSupply();
+    }
+    
+    receive() external payable virtual {
+    emit PaymentReceived(_msgSender(), msg.value);
     }
 
-    modifier onlyProductOwner(){
-        require(ERC721(nft).balanceOf(msg.sender) > 0);
-        _;
+    function totalReleased() public view returns (uint256) {
+    return _totalReleased;
     }
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
+    function released(uint256 tokenId) public view returns (uint256) {
+    return _released[tokenId];
     }
 
-    function mint(address _to, uint256 _mintAmount) public payable {
-        uint256 supply = totalSupply();
-        require(!paused);
-        require(_mintAmount > 0);
-        require(_mintAmount <= maxMintAmount);
+    function payee(uint256 tokenId) public view returns (address) {
+    return ERC721(nft).ownerOf(tokenId);
+    }
 
-        if (msg.sender != owner()) {
-                    require(msg.value >= cost * _mintAmount);
-                } 
+    function releasable(uint256 tokenId) public view returns (uint256) {
+    uint256 totalReceived = address(this).balance + totalReleased();
+    return _pendingPayment(totalReceived, released(tokenId));
+    }
 
-        for (uint256 i = 1; i <= _mintAmount; i++) {
-            _safeMint(_to, supply + i);
+    function release(uint256 tokenId) public payable virtual {
+    require(msg.sender == payee(tokenId), "sup");
+    uint256 payment = releasable(tokenId);
+    require(payment != 0, "PaymentSplitter: account is not due payment");
+        _totalReleased += payment;
+        unchecked {
+            _released[tokenId] += payment;
         }
-    }
-
-    function walletOfOwner(address _owner)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        uint256 ownerTokenCount = balanceOf(_owner);
-        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-        for (uint256 i; i < ownerTokenCount; i++) {
-            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        Address.sendValue(account, payment);
+        emit PaymentReleased(account, payment);
         }
-        return tokenIds;
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        require(
-            _exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-
-        string memory currentBaseURI = _baseURI();
-        return
-            bytes(currentBaseURI).length > 0
-                ? string(
-                    abi.encodePacked(
-                        currentBaseURI,
-                        tokenId.toString(),
-                        baseExtension
-                    )
-                )
-                : "";
-    }
-
-    //only owner
-    function setCost(uint256 _newCost) public onlyOwner {
-        cost = _newCost;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public onlyOwner override(ERC721) {
-        _transfer(from, to, tokenId);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    ) public onlyOwner override(ERC721) {
-        _safeTransfer(from, to, tokenId, data);
-    }
-
-    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-        maxMintAmount = _newmaxMintAmount;
-    }
-
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        baseURI = _newBaseURI;
-    }
-
-    function setBaseExtension(string memory _newBaseExtension)
-        public
-        onlyOwner
-    {
-        baseExtension = _newBaseExtension;
-    }
-
-    function pause(bool _state) public onlyOwner {
-        paused = _state;
-    }
-
-    function withdraw(uint256 x) public payable onlyProductOwner() {
-        (bool success, ) = payable(payment).call{
-            value: x*cost
-        }("");
-        require(success);
-    }
-
+        
+    function _pendingPayment(
+    uint256 totalReceived,
+    uint256 alreadyReleased
+    ) private view returns (uint256) {
+        return totalReceived / number - alreadyReleased;
+        }
 
 }
